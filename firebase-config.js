2edcd66a2c0e054846ddee767024ca6434f0a2fc
Daniel1571 <alfrelolivares2003@gmail.com>
2025-11-27 12:22:36 -0600

    Refactor Firebase config and initialization
    Removed unused imports and exported the app instance.

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // Importar SDKs de Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 // Configuración de Firebase
 const firebaseConfig = {
@@ -14,6 +14,5 @@ const firebaseConfig = {
 };
 
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export { collection, addDoc };
